4ecb4ba8ba195c1d57dc57e0cad07c80736935c9
YUSUF CAN AKIER <83148529+yusufCanAkier@users.noreply.github.com>
2022-06-19 15:08:28 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 extern crate csv;
 
 use std::io;
+use csv::Writer;
+
+fn example() -> Result<(), Box<Error>> {
+    let mut wtr = Writer::from_path("foo.csv")?;
+    wtr.write_record(&["a", "b", "c"])?;
+    wtr.write_record(&["x", "y", "z"])?;
+    wtr.flush()?;
+    Ok(())
+}
 
 fn main() {
     let mut rdr = csv::Reader::from_reader(io::stdin());
